8e5ad7744d567878b071c684479d5bc761d5e5c8
svcodes <61701181+svcodes@users.noreply.github.com>
2020-10-06 17:18:38 -0500

    Rocks Papers And Scissors
    very bad game making skills

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-# program that gets hypixel stats and stuff
-# must do "pip install -u requests" to use
-import requests
+import random
+options = ['rock','paper','scissors']
+computer_option = random.choice(options)
+player_option = None
+while player_option not in options:
+  player_option = input("Choose either rock, paper, or scissors: ")
+if player_option == computer_option:
+  print("Tie!")
+  print(f"Computer chose {computer_option}")
+elif player_option == 'rock' and computer_option == 'paper':
+  print("Computer wins!")
+  print(f"Computer chose {computer_option}")
+elif player_option == 'paper' and computer_option == 'rock':
+  print("You win!")
+  print(f"Computer chose {computer_option}")
+elif player_option == 'paper' and computer_option == 'scissors':
+  print("You win!")
+  print(f"Computer chose {computer_option}")
+elif player_option == 'scissors' and computer_option == 'paper':
+  print("Computer wins!")
+  print(f"Computer chose {computer_option}")
+elif player_option == 'rock' and computer_option == 'scissors':
+  print("You win!")
+  print(f"Computer chose {computer_option}")
+elif player_option == 'scissors' and computer_option == 'rock':
+  print("Computer wins!")
+  print(f"Computer chose {computer_option}")
 
-username = input("What is your minecraft username? ")
-data = requests.get(f'https://api.slothpixel.me/users/{username}).json()
-                    
-if data['error']:
-    print("bad")
-                    
