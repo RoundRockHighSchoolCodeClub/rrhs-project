ad40a19036c8c09f3f49927f1a44e1b7739c9407
svcodes <61701181+svcodes@users.noreply.github.com>
2020-10-06 16:53:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Code here!
+# program that gets hypixel stats and stuff
+# must do "pip install -u requests" to use
+import requests
+
+username = input("What is your minecraft username? ")
+data = requests.get(f'https://api.slothpixel.me/users/{username}).json()
+                    
+if data['error']:
+    print("bad")
+                    
