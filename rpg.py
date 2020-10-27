d543844954e7a0c3430ca5ef3f1151726f028ab8
Chase Fleeting <darthfleeting@gmail.com>
2020-10-27 17:59:46 -0500

    Update rpg.py to Python 3.x from Python 2.x

--- rpg.py
+++ rpg.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from random import randint
 
 class Character:
@@ -8,10 +9,10 @@ class Character:
   def do_damage(self, enemy):
     damage = min(
         max(randint(0, self.health) - randint(0, enemy.health), 0),
-        enemy.health) #Change hit chance
+        enemy.health)
     enemy.health = enemy.health - damage
-    if damage == 0: print "%s evades %s's attack." % (enemy.name, self.name)
-    else: print "%s hurts %s for %s damage!" % (self.name, enemy.name, damage)
+    if damage == 0: print ("%s evades %s's attack." % (enemy.name, self.name))
+    else: print ("%s hurts %s for %s damage!" % (self.name, enemy.name, damage))
     return enemy.health <= 0
 
 class Enemy(Character):
@@ -25,38 +26,37 @@ class Player(Character):
     Character.__init__(self)
     self.state = 'normal'
     self.health = 10
-    self.health_max = 10 #MAX HEALTH
+    self.health_max = 10
   def quit(self):
-    print "%s can't find the way back home, and dies of starvation.\nR.I.P." % self.name
+    print ("%s can't find the way back home, and dies of starvation.\nR.I.P." % self.name)
     self.health = 0
-  def help(self): print Commands.keys()
-  def status(self): print "%s's health: %d/%d" % (self.name, self.health, self.health_max)
+  def help(self): print (Commands.keys())
+  def status(self): print ("%s's health: %d/%d" % (self.name, self.health, self.health_max))
   def tired(self):
-    print "%s feels tired." % self.name
+    print ("%s feels tired." % self.name)
     self.health = max(1, self.health - 1)
   def rest(self):
-    if self.state != 'normal': print "%s can't rest now!" % self.name; self.enemy_attacks()
+    if self.state != 'normal': print ("%s can't rest now!" % self.name, self.enemy_attacks())
     else:
-      print "%s rests." % self.name
+      print ("%s rests." % self.name)
       if randint(0, 1):
         self.enemy = Enemy(self)
-        print "%s is rudely awakened by a %s!" % (self.name, self.enemy.name)
+        print ("%s is rudely awakened by a %s!" % (self.name, self.enemy.name))
         self.state = 'fight'
         self.enemy_attacks()
       else:
         if self.health < self.health_max:
           self.health = self.health + 1
-        else: print "%s slept too much." % self.name; self.health = self.health - 1
+        else: print ("%s slept too much." % self.name); self.health = self.health - 1
   def explore(self):
     if self.state != 'normal':
-      print "%s is too busy right now!" % self.name
-    
-      self.enemy_attacks()
+      print ("%s is too busy right now!" % self.name )
+     # self.enemy_attacks() Do not uncomment this. Not having an enemy loaded will cause this to err. 
     else:
-      print "%s explores a twisty passage." % self.name
+      print ("%s explores a twisty passage." % self.name)
       if randint(0, 1):
         self.enemy = Enemy(self)
-        print "%s encounters a %s!" % (self.name, self.enemy.name)
+        print ("%s encounters a %s!" % (self.name, self.enemy.name))
         self.state = 'fight'
       else:
         if randint(0, 1): self.shop()
@@ -64,39 +64,40 @@ class Player(Character):
             if randint(0, 1): self.tired()
   def shop(self):
       self.state = 'buy'
-      print "%s finds a shop! They are selling health potions for 2 coins." % (self.name)
+      print ("%s finds a shop! They are selling health potions for 2 coins." % (self.name))
   def buy(self):
-    if self.state != 'buy': print "%s offers coins to the wind." % (self.name)
+    if self.state != 'buy': print ("%s offers coins to the wind." % (self.name))
     else:
-        print "%s buys a health potion!" % (self.name)
+        print ("%s buys a health potion!" % (self.name))
         self.state = 'normal'
   def leave(self):
-      if self.state != 'buy': print "%s takes a singular step foward." % (self.name)
+      if self.state != 'buy': print ("%s takes a singular step foward." % (self.name))
       else:
           self.state = 'normal'
-          print "%s leaves the shop wondering if they will ever be back." % (self.name)
+          print ("%s leaves the shop wondering if they will ever be back." % (self.name))
   def flee(self):
-    if self.state != 'fight': print "%s runs in circles for a while." % self.name; self.tired()
+    if self.state != 'fight': print ("%s runs in circles for a while." % self.name); self.tired()
     else:
       if randint(1, self.health + 5) > randint(1, self.enemy.health):
-        print "%s flees from %s." % (self.name, self.enemy.name)
+        print ("%s flees from %s." % (self.name, self.enemy.name))
         self.enemy = None
         self.state = 'normal'
-      else: print "%s couldn't escape from %s!" % (self.name, self.enemy.name); self.enemy_attacks()
+      else: print ("%s couldn't escape from %s!" % (self.name, self.enemy.name)); self.enemy_attacks()
   def attack(self):
-    if self.state != 'fight': print "%s swats the air, without notable results." % self.name; self.tired()
+    if self.state != 'fight': print ("%s swats the air, without notable results." % self.name); self.tired()
     else:
       if self.do_damage(self.enemy):
-        print "%s executes %s!" % (self.name, self.enemy.name)
+        print ("%s executes %s!" % (self.name, self.enemy.name))
         self.enemy = None
         self.state = 'normal'
         if randint(0, self.health) < 10:
           self.health = self.health + 1
           self.health_max = self.health_max + 1
-          print "%s feels stronger!" % self.name
+          print ("%s feels stronger!" % self.name)
       else: self.enemy_attacks()
   def enemy_attacks(self):
-    if self.enemy.do_damage(self): print "%s was slaughtered by %s!!!\nR.I.P." %(self.name, self.enemy.name)
+    if self.enemy.do_damage(self):
+         print ("%s was slaughtered by %s!!!\nR.I.P." %(self.name, self.enemy.name))
 
 Commands = {
   'quit': Player.quit,
@@ -108,16 +109,16 @@ Commands = {
   'attack': Player.attack,
   'buy': Player.buy,
   'leave': Player.leave,
-  # Add more commands or whatever
+  #add more commands here
   }
 
 p = Player()
-p.name = raw_input("What is your character's name? ")
-print "(type help to get a list of actions)\n"
-print "%s enters a dark cave, searching for adventure." % p.name
+p.name = input("What is your character's name? ")
+print ("(type help to get a list of actions)\n")
+print ("%s enters a dark cave, searching for adventure." % p.name)
 
 while(p.health > 0):
-  line = raw_input("> ")
+  line = input("> ")
   args = line.split()
   if len(args) > 0:
     commandFound = False
@@ -127,4 +128,4 @@ while(p.health > 0):
         commandFound = True
         break
     if not commandFound:
-      print "%s doesn't understand the suggestion." % p.name
+      print ("%s doesn't understand the suggestion." % p.name)
